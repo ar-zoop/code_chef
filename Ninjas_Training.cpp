4b944e0062fcbca85dc1ebee5df1a85c1bd8bd36
arzoop <arzoobapna01@gmail.com>
2022-06-23 23:51:29 +0530

    ninjas training

--- Ninjas_Training.cpp
+++ Ninjas_Training.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int memo[100000][100000];
 int sub(int idx, int task, vector<vector<int>> &points, int n){
     int left,right;
     //base case
@@ -8,6 +9,7 @@ int sub(int idx, int task, vector<vector<int>> &points, int n){
     if(idx>n-1){
         return 0;
     }
+    if(memo[idx][task]!=-1)return memo[idx][task];
      if(task==0){
         //left    
         left= sub(idx+1,2, points, n )+points[idx][task];
@@ -31,13 +33,14 @@ int sub(int idx, int task, vector<vector<int>> &points, int n){
         right= sub(idx+1,1, points, n )+points[idx][task];
         
     }
-    return max(left,right);
+    return memo[idx][task]=max(left,right);
 }
 
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
     // Write your code here.
     int max=INT_MIN;
+    memset(memo, -1, sizeof(memo));
     for (int i=0;i<3;i++){
         int jump=sub(0,i, points, n);
         if(jump>max)max=jump;
