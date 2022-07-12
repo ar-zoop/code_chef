6c8e8b29b6045560b56c5ce67186e65ac1c32aa2
arzoop <arzoobapna01@gmail.com>
2022-07-12 21:19:03 +0530

    cletab

--- cletab.cpp
+++ cletab.cpp
@@ -1,23 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        map <int, int> mpp;
-        map<int,int>::iterator itr;
-        int n,m;
-        cin>>n>>m;
-        int arr[400], e_counter=0,count=0;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    int c[m];
+        int chairs[n]={0};
+	    map<int,int>map_c;
+	    for(int i=0;i<m;i++){
+	        cin>>c[i];
+	        if(map_c.find(c[i])!=map_c.end()){
+	            map_c[c[i]]++;
+	        }
+	        else{
+	            map_c[c[i]]=1;
+	        }
+	    }
+        int count=0;
         for(int i=0;i<m;i++){
-            cin>>arr[i];
-        }
-        for (int i=0;i<n;i++){
-            if(!mpp.find(arr[i])){
-                mpp[arr[i]]=1;
-                count++;
+            int mini=INT_MAX,j,ele;
+            for(j=0;j<n;j++){                
+                if(mini>map_c[chairs[j]]){
+                    mini=map_c[chairs[j]];
+                    ele=j;
+                }
             }
+            chairs[j]=c[i];
+            map_c[c[i]]--;
+            count++;     
         }
-    }
-    return 0;
+        cout<<count<<endl;
+	}
+	return 0;
 }
