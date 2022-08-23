01b30d7e50d9625aa3e2e05bedfaad15eaf30de1
arzoop <arzoobapna01@gmail.com>
2022-08-23 17:46:16 +0530

    kadanes algorithm

--- leetcode_maximum_subarray.cpp
+++ leetcode_maximum_subarray.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int sub(int idx, int mustTake,vector<int>& nums,vector<vector<int>> &memo){
-        int take=INT_MIN, not_take=INT_MIN;
-        //base case
-        if(idx==(nums.size()-1)) {
-            return nums[idx];
-        }
-        if(memo[idx][mustTake]!=-1)return memo[idx][mustTake];
-        //cases
-        if(mustTake==true){
-            take=sub(idx+1, true, nums,memo)+nums[idx];
-        }
-        else{            
-            not_take=sub(idx+1,false, nums, memo)+0;
-            take=sub(idx+1, true, nums, memo)+nums[idx];
-        }
-        cout<<max(not_take,take)<<" " ;
-        return memo[idx][mustTake]=max(not_take,take);
-    }
     int maxSubArray(vector<int>& nums) {
-        vector<vector<int>> memo{10000, vector<int>(2, -1)};
-        return( sub(0, false, nums,memo));
-        
+        int max_sum=INT_MIN, sum=0, start=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[start]<0) {
+                max_sum=max(max_sum, nums[start]);
+                start++;
+            }
+            else{
+                sum+=nums[i];
+                max_sum=max(sum, max_sum);
+                if(sum<0){
+                    start=i+1;
+                    sum=0;
+                }
+            }
+        }
+        return max_sum;
     }
 };
