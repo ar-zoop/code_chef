2ee2efe45631f6c3d0ec3441d5fc4cfc930d3b30
arzoop <arzoobapna01@gmail.com>
2022-08-28 17:51:23 +0530

    combination

---
+++ leetcode_combination.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sub(int idx, vector<int>& ds, int r, int n, vector<vector<int>> &res ){
+        if(ds.size()==r){
+            res.push_back(ds);
+            return ;
+        }
+        if(idx>n) return;
+        
+        //take
+        ds.push_back(idx);
+        sub(idx+1, ds, r, arr, n, res);
+        //not take
+        ds.pop_back();
+        sub(idx+1, ds, r, arr, n, res);
+        
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> ds;
+        sub(1,ds, k,n, res);
+        return res;
+    }
+};
