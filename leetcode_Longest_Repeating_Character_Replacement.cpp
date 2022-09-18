6bcc749a9096e4d8787a854a7bfa7a3d029f2360
arzoop <arzoobapna01@gmail.com>
2022-09-18 09:41:38 +0530

    Longest Repeating Character Replacement

--- leetcode_Longest_Repeating_Character_Replacement.cpp
+++ leetcode_Longest_Repeating_Character_Replacement.cpp
@@ -1,38 +1,39 @@
-bool compare(vector<int> v1, vector<int> v2){
-        return v1[0]>v2[0];
-}
 class Solution {
 public:
-    
     int characterReplacement(string s, int k) {
-        map<pair<int,char>,int> mpp;
-        int sptr=0, count=0;
-        for(int i=0 ;i <s.size(); i++){
-            if (i>0 && s[i]!=s[i-1]) sptr++;
-            
-            if(mpp.find(make_pair(sptr,s[i]))==mpp.end()){
-                mpp[make_pair(sptr,s[i])]=1;
-                count++;
+        int sum=0, max0=0, sptr=0, maxi=0,eptr=0;
+        map<char,int> freq;
+        while(eptr<s.length()){
+            if(sptr>eptr){
+                sptr=eptr;
+                eptr++;
             }
             
-            else if(i>0 && s[i]==s[i-1]){
-                mpp[make_pair(sptr,s[i])]++;
+            if(freq.find(s[eptr])==freq.end()){
+                freq[s[eptr]]=1;                 
             }
+            else{
+                freq[s[eptr]]++;                 
+            }
+            sum++;
+            if(freq[s[eptr]]>max0){
+                max0=freq[s[eptr]];               
+            }
+            if(sum-max0<=k){
+                maxi=max(sum,maxi);
+            }
+            else{
+                while(sum-max0>k && sptr<=eptr){
+                    freq[s[sptr]]--;
+                    sptr++;
+                    sum--;
+                    auto pr = std::max_element(freq.begin(), freq.end(), [](const auto &x, const auto &y) {
+                        return x.second < y.second;
+                    });
+                }
+            }
+            eptr++;
         }
-         map<pair<int,char>,int> :: iterator imp;
-        
-        vector<vector<int>> vec( count , vector<int> (2, 0)); 
-        int i=0;
-        for(imp=mpp.begin(); imp!=mpp.end(); imp++){
-            // cout<<(imp->first).second <<" "<<imp->second<<endl;
-            vec[i][0]=imp->second;
-            vec[i][1]=(imp->first).second;
-            i++;
-        }
-        sort(vec.begin(), vec.end(), compare);
-        for(int i=0 ; i<vec.size(); i++){
-            cout<<vec[i][0]<<"="<<vec[i][1]<<endl;
-        }
-        return 0;
+        return maxi;
     }
 };
