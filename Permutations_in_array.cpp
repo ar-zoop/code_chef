3d9bee2e070b9b973ac32695c88ba193ec225676
ar-zoop <arzoobapna01@gmail.com>
2022-10-20 11:25:32 +0530

    Permutations in array

--- Permutations_in_array.cpp
+++ Permutations_in_array.cpp
@@ -1,13 +1,12 @@
-class Solution {
-  public:
+public:
     bool isPossible(long long a[], long long b[], int n, long long k) {
         // Your code goes here
-        sort(a,a+n, greater <int>());
+        sort(a,a+n);
         sort(b,b+n, greater<int>());
-        // for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             // cout<<a[i]<<" "<<b[i]<<endl;
-            if(a[0]+b[0]>=k) return true;
-        
+            if(a[i]+b[i]>=k) return true;
+        }
         return false;
     }
 };
