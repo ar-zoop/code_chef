e7aa43ac9f071b3f8c397d32d05815f9c730aaed
ar-zoop <arzoobapna01@gmail.com>
2022-10-22 23:15:43 +0530

    Find a common element in all rows of a matrix

--- Find_a_common_element_in_all_rows_of_a_given_row-wise_sorted_matrix.cpp
+++ Find_a_common_element_in_all_rows_of_a_given_row-wise_sorted_matrix.cpp
@@ -13,12 +13,12 @@ int findCommon(vector<vector<int>>&matrix, int mat_size, int mat_0_size){
     }
     for(int i=1; i<mat_size;i++){
         for(int j=0;j<mat_0_size;j++){
-           if(row_one.find(matrix[i][j])!=row_one.end())
+        //    if(row_one.find(matrix[i][j])!=row_one.end())
+            if(matrix[i][j] != matrix[i][j - 1])
                 row_one[matrix[i][j]]++;
         }
     }
     for(auto x:row_one){
-        // cout<<x.second<<endl;
         if(x.second == mat_0_size-1){
             return x.first;
         }
@@ -28,8 +28,8 @@ int findCommon(vector<vector<int>>&matrix, int mat_size, int mat_0_size){
 
 int main(){
     vector<vector<int>> matrix={ {1, 2, 3, 4, 5},
-                    {2, 4, 5, 8, 10},
-                    {3, 5, 7, 9, 11},
+                    {5, 5, 10, 10, 10},
+                    {3, 10, 7, 9, 11},
                     {1, 3, 5, 7, 9},
                   };
     cout<< findCommon(matrix, matrix.size(), matrix[0].size());
