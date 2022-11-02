322824ee9261ce91fe8749e5dde0a5d1e4ef2ecf
ar-zoop <arzoobapna01@gmail.com>
2022-11-02 20:36:27 +0530

    opti: Median of 2 Sorted Arrays of Different Sizes

--- Median_of_2_Sorted_Arrays_of_Different_Sizes.cpp
+++ Median_of_2_Sorted_Arrays_of_Different_Sizes.cpp
@@ -1,42 +1,56 @@
-class Solution{
-    public:
+class Solution
+{
+public:
     int mid_pos;
-    
-    double merge(vector<int>& array1, vector<int>& array2, int n, int m, int mid){
-        vector <int> arr(n+m);
-        int left=0, right=0, k=-1;
-        while(k<mid && left<n && right<m){
-            if(array2[right]<array1[left]){
-                arr[++k]=array2[right];
+
+    double merge(vector<int> &array1, vector<int> &array2, int n, int m, int mid)
+    {
+        int left = 0, right = 0, k = -1;
+        int mid_prev = -1, mid_main = -1;
+        while (k < mid && left < n && right < m)
+        {
+            if (array2[right] < array1[left])
+            {
+                mid_prev = mid_main;
+                mid_main = array2[right];
+                k++;
                 right++;
             }
-            else{
-                arr[++k]=array1[left];
+            else
+            {
+                mid_prev = mid_main;
+                mid_main = array1[left];
+                k++;
                 left++;
             }
         }
-        while(k<mid && left<n){
-            arr[++k]=array1[left];
+        while (k < mid && left < n)
+        {
+            mid_prev = mid_main;
+            mid_main = array1[left];
+            k++;
             left++;
         }
-        while(k<mid && right<m){
-            arr[++k]=array2[right];
+        while (k < mid && right < m)
+        {
+            mid_prev = mid_main;
+            mid_main = array2[right];
+            k++;
             right++;
         }
-        if((n+m) %2==0){
-            return (arr[k]+arr[k-1])/2.0;
+        if ((n + m) % 2 == 0)
+        {
+            return (mid_main + mid_prev) / 2.0;
         }
-        return arr[k];
-        
+        return mid_main;
     }
-    
-    double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+
+    double MedianOfArrays(vector<int> &array1, vector<int> &array2)
     {
         // Your code goes here
-        int n=array1.size();
-        int m= array2.size();
-        mid_pos=(n+m)/2;
-        return merge(array1, array2,n,m, mid_pos);
-    
+        int n = array1.size();
+        int m = array2.size();
+        mid_pos = (n + m) / 2;
+        return merge(array1, array2, n, m, mid_pos);
     }
 };
