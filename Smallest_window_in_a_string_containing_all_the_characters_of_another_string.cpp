39b28968e44c2cd6b6b8c5744edbbb2e9fc244d3
ar-zoop <arzoobapna01@gmail.com>
2022-11-19 21:07:29 +0530

    Smallest window in a str with all char of str 2

--- Smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
+++ Smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
@@ -6,64 +6,48 @@ class Solution
     string smallestWindow (string s, string p)
     {
         // Your code here
-        int start=0, end=0;
+        int start=0, end=0, count=p.length(), s_len=s.length(), min_win_size=INT_MAX, p_len=p.length();
         string res="-1";
-        int res_len=INT_MAX;
-        map<char,int> need;
-        map<char, int> have;
-        int s_size=s.length();
-        int count=p.size();
-        for(int i=0 ;i<count; i++){
-            if(need.find(p[i])==need.end()){
-                need[p[i]]=1;
-            }
-            else{
-                p[i]++;
-            }
+        vector<char>need(26, 0), have(26,0);
+        // map<char,  int> need, have;
+        for(int i=0; i<p_len; i++){
+            need[p[i]-'a']++;
         }
-        while(start<s_size){
-            if(count>0){
-                if(have.find(s[end])==have.end()){
-                    have[s[end]]=1;
-                }
-                else{
-                    have[s[end]]++;
-                }
-                if(need.find(s[end])!=need.end() && need[s[end]]>=have[s[end]] ){
-                    count--;
-                }
-                if(count!=0)
-                    end++;
-            }
-            if(count==0){
-                if(res=="-1"){
-                    res.assign(s,start,end);
-                    res_len=end-start+1;
-                }
-                if(need.find(s[start])==need.end()){
-                    have[s[start]]--;
-                }
-                else{
-                    if(need[s[start]]<have[s[start]]){
-                        have[s[start]]--;
-                    }
-                    else{
-                        if(res_len>(end-start+1)){
-                            res.assign(s,start,end);
-                            res_len=res.length();
-                        }
-                        count++;
-                        have[s[start]]--;
-                        end++;
+        while(true){
+            bool f1=false, f2=false;
+            while(end<s_len && count>0){
+                f1=true;
+                if(need[s[end]-'a']>0){
+                    have[s[end]-'a']++;
+                    if(have[s[end]-'a']<=need[s[end]-'a']){
+                        count--;
                     }
                 }
-                start++;
+                end++;
             }
-            
+            while(start<=end  && count==0){
+                f2=true;
+                if(need[s[start]-'a']>0 && need[s[start]-'a']<have[s[start]-'a']){
+                    have[s[start]-'a']--;
+                    start++;
+                }
+                
+                else if(need[s[start]-'a']>0 && need[s[start]-'a']==have[s[start]-'a']){
+                    have[s[start]-'a']--;
+                    start++;
+                    count++;
+                    if((end-start)<min_win_size){
+                        res.assign(s.begin()+(start-1), s.begin()+end);
+                        min_win_size=end-start;
+                    }
+                }
+                
+                else{
+                    start++;
+                }
+            }
+            if(f1==false && f2==false) break;
         }
-        
         return res;
-        
-        
     }
 };
