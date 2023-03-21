8a359a5ea040db4d86691ecd3066235fcbc0f072
ar-zoop <arzoobapna01@gmail.com>
2023-03-21 17:30:31 +0530

    Remove duplicates from an unsorted linked list

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,92 +1,50 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
+
 // } Driver Code Ends
-// User function Template for C++
-class Solution {
-  public:
-    vector<vector<string>> mergeDetails(vector<vector<string>>& details) {
-        // code here
-        int N=details.size();
-        int parent[N];
-        for(int i=0; i<N; i++){
-            parent[i]=i;
-        }
-        map<string,int> dict;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<details[i].size(); j++){
-                if(j==0) continue;
-                else{
-                    if(dict.find(details[i][j])==dict.end()){
-                        dict[details[i][j]]=i;
-                    }
-                    else{
-                        parent[i]=dict[details[i][j]];
-                    }
-                }
-            }
-        }
-        map<string, int>:: iterator itr= dict.begin();
-        
-        while(itr!=dict.end()){
-            cout<<itr->first<<" "<<itr->second<<endl;
-        }
-    return details;
-    }
+//User function template for C++
+bool comp(string &a, string &b){
+    string c=a+b, d=b+a;
+    if(c>=d) return true;
+    return false;
+}
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	
+	string printLargest(vector<string> &arr) {
+	    // code here
+	   // sort(arr.begin(), arr.end(), comp);
+	    sort(arr.begin(), arr.end(), comp);
+	    string temp="";
+	   // for(int i=0; i<arr.size(); i++){
+	   //     temp+=arr[i];
+	   // }
+	    return temp;
+	}
 };
 
-
 //{ Driver Code Starts.
+
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
+        int n, i;
         cin >> n;
-        vector<vector<string>> adj;
-
-        for (int i = 0; i < n; i++) {
-            vector<string> temp;
-            string s;
-            cin >> s;
-            temp.push_back(s);
-            int x;
-            cin >> x;
-
-            for (int j = 0; j < x; j++) {
-                string s1;
-                cin >> s1;
-                temp.push_back(s1);
-            }
-            adj.push_back(temp);
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-
-        Solution obj;
-        vector<vector<string>> res = obj.mergeDetails(adj);
-        sort(res.begin(),res.end(),[](const vector<string>& a, const vector<string>& b){
-            return a[0]<=b[0];
-        });
-        cout << "[";
-        for (int i = 0; i < res.size(); ++i) {
-            cout << "[";
-            for (int j = 0; j < res[i].size(); j++) {
-                if (j != res[i].size() - 1)
-                    cout << res[i][j] << ","
-                         << " ";
-                else
-                    cout << res[i][j];
-            }
-            if (i != res.size() - 1)
-                cout << "], ";
-            else
-                cout << "]";
-        }
-        cout << "]"
-             << "\n";
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
     }
+    return 0;
 }
-
 // } Driver Code Ends
